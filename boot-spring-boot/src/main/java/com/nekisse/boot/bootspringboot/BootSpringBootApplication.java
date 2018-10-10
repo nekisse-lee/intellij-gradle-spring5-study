a9ca2448a6e6365d89dc438bc69ed6aa27129542
nekisse <lsh891224@gmail.com>
2018-10-10 20:29:52 +0900

    스프링 공부

---
+++ boot-spring-boot/src/main/java/com/nekisse/boot/bootspringboot/BootSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.nekisse.boot.bootspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSpringBootApplication.class, args);
+    }
+}
---
+++ spring5/src/main/java/com/nekisse/spring5/chap02/Greeter.java
@@ -0,0 +1,15 @@
+package com.nekisse.spring5.chap02;
+
+public class Greeter {
+
+    private String format;
+
+    public String greet(String guest) {
+        return String.format(format, guest);
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+}
