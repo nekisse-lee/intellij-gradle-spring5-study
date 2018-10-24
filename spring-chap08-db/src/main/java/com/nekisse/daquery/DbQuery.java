5056e2a241977d4a236cd9dae23cfc455267b118
nekisse <lsh891224@gmail.com>
2018-10-24 18:04:31 +0900

    스프링 db

---
+++ spring-chap08-db/src/main/java/com/nekisse/daquery/DbQuery.java
@@ -0,0 +1,37 @@
+package com.nekisse.daquery;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbQuery {
+
+    private DataSource dataSource;
+
+    public DbQuery(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public int count() {
+        Connection conn = null;
+        try {
+            conn = dataSource.getConnection();
+            try (Statement stmt = conn.createStatement();  //풀에서 구함
+                 ResultSet rs = stmt.executeQuery("select count(*) from MEMBER")) {
+                rs.next();
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (conn != null)
+                try {
+                    conn.close();  //풀에 반환
+                } catch (SQLException e) {
+                }
+        }
+    }
+
+}
